8a7d5a3194973e153300cd7ce667e8e70c6998f7
gap <devel@gap-system.org>
1999-06-04 11:59:35 +0000

    spacegrps.* renamed to spacegrp.*

--- grp/spacegrp.gd
+++ grp/spacegrp.gd
@@ -1,3 +1,13 @@
+#############################################################################
+##
+#A  spacegrp.gd             CrystGap library                     Bettina Eick
+#A                                                              Franz G"ahler
+#A                                                              Werner Nickel
+##
+#Y  Copyright 1997-1999  by  Bettina Eick,  Franz G"ahler  and  Werner Nickel
+##
+##  Declarations for IT space groups
+##
 
 #############################################################################
 ##
